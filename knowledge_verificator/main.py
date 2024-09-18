c3f34bf6c8ff2e4c577b3f51d06c035300f28c66
Iamhexi <igor@sosnowicz.eu>
2024-09-18 20:39:51 +0000

    feat(qg): implement basic QG module

--- knowledge_verificator/main.py
+++ knowledge_verificator/main.py
@@ -1,1 +1,19 @@
 """Main module with CLI definition."""
+<<<<<<< HEAD
+||||||| parent of 0627c06 (feat(qg): implement basic QG module)
+from knowledge_verificator.nli import infer_relation
+
+if __name__ == '__main__':
+    premise = input("Premise: ")
+    hypothesis = input("Hypothesis: ")
+    print(
+        infer_relation(
+            premise=premise,
+            hypothesis=hypothesis
+        ).value
+    )
+=======
+
+if __name__ == '__main__':
+    print('Currently nothing happens here.')
+>>>>>>> 0627c06 (feat(qg): implement basic QG module)
