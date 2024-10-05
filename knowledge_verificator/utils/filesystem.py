b43461a3c3cec142f2d72193174bdea5b112c18a
Iamhexi <igor@sosnowicz.eu>
2024-10-05 11:56:03 +0200

    tests(test_qg): implement simple model performance measure

--- knowledge_verificator/utils/filesystem.py
+++ knowledge_verificator/utils/filesystem.py
@@ -18,3 +18,26 @@ def in_directory(file: Path, directory: Path) -> bool:
     return str(directory.resolve()) in str(
         file.resolve()
     ) and not file.samefile(directory)
+
+
+def create_text_file(path: Path | str, content: str = '') -> None:
+    """
+    Create a text file in the supplied location.
+
+    If no content is provided, an empty file is created.
+
+    Args:
+        path (Path | str): Location of a new file.
+        content (str, optional): Content of a file. Defaults to ''.
+
+    Raises:
+        FileExistsError: Raised if a file already exists.
+    """
+    if isinstance(path, str):
+        path = Path(path)
+
+    if path.exists():
+        raise FileExistsError(f'A file already exists under `{str(path)}`')
+
+    with open(path.resolve(), 'wt', encoding='utf-8') as fd:
+        fd.write(content)
