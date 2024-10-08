5aee22918a2d492947fba775106e5f0a5d10f5f5
Iamhexi <igor@sosnowicz.eu>
2024-10-08 15:21:18 +0200

    feat(main, command_line): reorganise to allow running different operating modes

---
+++ knowledge_verificator/command_line.py
@@ -0,0 +1,123 @@
+"""Module with an interactive command-line interface."""
+
+from rich.text import Text
+
+from knowledge_verificator.io_handler import logger, console, config
+from knowledge_verificator.answer_chooser import AnswerChooser
+from knowledge_verificator.materials import MaterialDatabase
+from knowledge_verificator.nli import NaturalLanguageInference, Relation
+from knowledge_verificator.qg import QuestionGeneration
+from knowledge_verificator.utils.menu import choose_from_menu
+
+
+def run_cli_mode():
+    """
+    Run an interactive command-line interface.
+
+    Raises:
+        ValueError:
+    """
+    qg_module = QuestionGeneration()
+    chooser = AnswerChooser()
+
+    while True:
+        options = ['knowledge database', 'my own paragraph']
+        user_choice = choose_from_menu(
+            menu_elements=options, plural_name='options'
+        )
+
+        match user_choice:
+            case 'knowledge database':
+                try:
+                    material_db = MaterialDatabase(config.learning_materials)
+                except FileNotFoundError:
+                    console.print(
+                        f'In the `{config.learning_materials}` there is no database. '
+                        'Try using your own materials.'
+                    )
+                    continue
+
+                if not material_db.materials:
+                    console.print(
+                        'The knowledge database exists but is empty. '
+                        'Try using your own materials.'
+                    )
+                    continue
+
+                material = choose_from_menu(
+                    material_db.materials,
+                    plural_name='materials',
+                    attribute_to_show='title',
+                )
+
+                if material is None:
+                    continue
+
+                paragraph = str(
+                    choose_from_menu(material.paragraphs, 'paragraphs')
+                )
+
+                if paragraph is None:
+                    continue
+
+                console.print('Learn this paragraph: ')
+                console.print(paragraph)
+                console.print()
+                input('Press ENTER when ready.')
+
+            case 'my own paragraph':
+                console.print('Enter a paragraph you would like to learn: ')
+                paragraph = input().strip()
+
+            case _:
+                console.print('Unrecognised option, try again!')
+
+        logger.debug('Loaded the following paragraph:\n %s', paragraph)
+
+        chosen_answer = chooser.choose_answer(paragraph=paragraph)
+        if not chosen_answer:
+            logger.error(
+                'The supplied paragraph is either too short or too general. '
+                'Please, try providing a longer or more specific paragraph.'
+            )
+            continue
+
+        console.clear()
+
+        logger.debug(
+            'The `%s` has been chosen as the answer, based on which question '
+            'will be generated.',
+            chosen_answer,
+        )
+
+        question_with_context = qg_module.generate(
+            answer=chosen_answer, context=paragraph
+        )
+        question = question_with_context['question']
+        logger.debug(
+            'Question Generation module has supplied the question: %s', question
+        )
+
+        console.print(
+            f'\nAnswer the question with full sentence. {question} \nYour answer: '
+        )
+        user_answer = input().strip()
+
+        nli_module = NaturalLanguageInference()
+        relation = nli_module.infer_relation(
+            premise=paragraph, hypothesis=user_answer
+        )
+
+        match relation:
+            case Relation.ENTAILMENT:
+                feedback = 'correct'
+                style = 'green'
+            case Relation.CONTRADICTION:
+                feedback = f'wrong. Correct answer is {chosen_answer}'
+                style = 'red'
+            case Relation.NEUTRAL:
+                feedback = 'not directly associated with the posed question'
+                style = 'yellow'
+
+        feedback_text = Text(f'Your answer is {feedback}.', style=style)
+        console.print(feedback_text)
