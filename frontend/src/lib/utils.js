3062adbe91039bae6e325785df02b1a607b1fce7
Iamhexi <igor@sosnowicz.eu>
2024-10-27 17:36:27 +0100

    feat(frontend): establish basic frontend flow

---
+++ frontend/src/lib/utils.js
@@ -0,0 +1,16 @@
+
+/**
+ * Show an error message.
+ * @param {string} message
+ */
+export function showErrorMessage(message) {
+    alert(`Error: ${message}.`);
+}
+
+export function enableLoader() {
+    // TODO: Implement loader.
+}
+
+export function disableLoader() {
+    // TODO: Implement disabling loader.
+}
