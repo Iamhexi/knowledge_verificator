e5220827a43ac5350f783395e91d5a5bee1baf04
Iamhexi <igor@sosnowicz.eu>
2024-10-28 22:19:20 +0100

    feat(frontend): improve and unify frontend styles

--- frontend/src/routes/evaluate/+page.svelte
+++ frontend/src/routes/evaluate/+page.svelte
@@ -53,14 +53,95 @@ onMount(async () => {
   evaluation = await evaluateAnswer(formData.context, formData.userAnswer);
   disableLoader();
 });
+
+
+	/**
+   * Generate a feedback header based on the evaluation.
+	 * @param {string} evaluation
+   * @returns {string|null}
+	 */
+function getHeader(evaluation) {
+  if (evaluation === 'entailment') {
+    return 'Correct answer';
+  } else if (evaluation === 'neutral') {
+    return 'Unassociated answer';
+  } else if (evaluation === 'contradiction') {
+    return 'Wrong answer';
+  }
+  return null;
+}
+
+/**
+ * Generate a feedback paragraph based on the evaluation.
+ * @param {string} evaluation
+ * @returns {string}
+ */
+ function getParagraph(evaluation) {
+  const feedbackMessages = {
+    entailment: 'Your answer is correct. Well done!',
+    neutral: 'Your answer does not seems to tackle the question. It is not necessarily wrong. Try to rephrase your answer.',
+    contradiction: 'Your answer is wrong. Revise the learning material and try again.'
+  };
+
+  return feedbackMessages[evaluation] || 'No evaluation provided. Please ensure that the evaluation is valid.';
+}
+
 </script>
 
-{#if formData.context && formData.userAnswer && formData.correctAnswer }
-  <p>The context: {formData.context}</p>
-  <p>Your answer: {formData.userAnswer}</p>
-  <p>The correct answer: {formData.correctAnswer}</p>
-  <p>Question: {formData.question}</p>
-  <p>Evaluation: {evaluation}</p>
+{#if evaluation }
+  <main class="feedback-wrapper">
+    <header><h1>Feedback</h1></header>
+    <p class="feedback-evaluation {evaluation}">
+      <b>{getHeader(evaluation)}</b>
+      <br>
+      {getParagraph(evaluation)}
+    </p>
+    <p><b>Question:</b> {formData.question}</p>
+    <p><b>Your answer:</b> {formData.userAnswer}</p>
+    <p><b>The correct answer:</b> {formData.correctAnswer}</p>
+    <p><b>The context:</b> {formData.context}</p>
+  </main>
 
   <a href="/">Change a paragraph</a>
 {/if}
+
+<style>
+  .feedback-wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+    justify-content: space-between;
+  }
+
+  .feedback-wrapper p {
+    padding: 1rem;
+    border: 1px solid black;
+  }
+
+  .feedback-evaluation {
+    border-radius: 5px;
+    text-align: center;
+    line-height: 2rem;
+    border-width: 1px;
+    border-style: solid;
+  }
+
+  .feedback-evaluation.entailment {
+    background-color: #d4edda;
+    color: #155724;
+    border: 1px solid #c3e6cb;
+  }
+
+  .feedback-evaluation.contradiction {
+    background-color: #f8d7da;
+    color: #721c24;
+    border: 1px solid #f5c6cb;
+  }
+
+  .feedback-evaluation.neutral {
+    background-color: #fff3cd;
+    color: #856404;
+    border: 1px solid #ffeeba;
+  }
+
+</style>
