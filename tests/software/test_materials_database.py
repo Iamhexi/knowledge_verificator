919a54939ebc9fee39ea0568893166212520cddd
Iamhexi <igor@sosnowicz.eu>
2024-11-17 12:26:02 +0100

    fix(tests): increase server start-up time to 90 seconds

--- tests/software/test_materials_database.py
+++ tests/software/test_materials_database.py
@@ -52,7 +52,7 @@ def server(mock_args, database_directory):
         kwargs={'host': SERVER, 'port': PORT, 'reload': False},
     )
     process.start()
-    wait_for_server_startup(timeout=30)
+    wait_for_server_startup(timeout=90)
 
     yield
 
